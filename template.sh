dd2fe26e50889fa2aa1b073b27b5e57de98fcdeb
Jayachadnra-v77 <jayachandra.v986@gmail.com>
2026-02-28 13:07:45 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,21 @@
+
+#Creating directory
+
+#-p stands for parameter
+
+mkdir -p src
+mkdir -p research
+
+#Creating files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch sepup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory and files are created sucessfully"
